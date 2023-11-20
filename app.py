6551e4b8e635df5b3af80393e0bbda388927b69a
Pavan Gudiwada <25551553+pavangudiwada@users.noreply.github.com>
2023-11-20 08:02:07 +0530

    Added Black code formatter

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
-import streamlit as st 
+import streamlit as st
 from os import listdir
 import logging
 
 logging.basicConfig(level=logging.DEBUG)
 
-image_names= listdir("images/")
-image_paths = ["images/"+ image for image in image_names]
+image_names = listdir("images/")
+image_paths = ["images/" + image for image in image_names]
 logging.debug("Imported Image Paths")
 
 st.set_page_config(page_title="Generative Art With Go")
 
-
+## TODO add option to filter the number of images being displayed
 st.title("Generative Art with Go!")
-st.markdown("A gallery to display images generated using Golang by Pavan Gudiwada. Code [here](https://github.com/pavangudiwada/generative-art-with-go)")
+st.markdown(
+    "A gallery to display images generated using Golang by Pavan Gudiwada. Code [here](https://github.com/pavangudiwada/generative-art-with-go)"
+)
 logging.debug("Displaying Images")
 st.image(image=image_paths)
