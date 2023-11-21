e02ba151fc51de46758def9c1d54d7e67eab8067
Pavan Gudiwada <25551553+pavangudiwada@users.noreply.github.com>
2023-11-21 10:25:35 +0530

    Added a slider for the number of images and a toggle for random image display

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from os import listdir
 import logging
+import random
+
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -10,10 +12,13 @@ logging.debug("Imported Image Paths")
 
 st.set_page_config(page_title="Generative Art With Go")
 
-## TODO add option to filter the number of images being displayed
 st.title("Generative Art with Go!")
 st.markdown(
     "A gallery to display images generated using Golang by Pavan Gudiwada. Code [here](https://github.com/pavangudiwada/generative-art-with-go)"
 )
 logging.debug("Displaying Images")
-st.image(image=image_paths)
+num_images = st.slider("Number of Images displayed", min_value=2, max_value=len(image_paths), step=1)
+random_images = st.toggle("Display Random images", value=True)
+if random_images:
+    random.shuffle(image_paths)
+st.image(image=image_paths[:num_images])
